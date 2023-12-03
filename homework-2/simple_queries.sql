585956d3aa5726796921560a2ff1a0e5ca897890
Zhenya Osokina <kapitan.kub@gmail.com>
2023-12-04 00:02:05 +0500

    готово, с пояснением по 5 заданию

--- homework-2/simple_queries.sql
+++ homework-2/simple_queries.sql
@@ -3,7 +3,7 @@
 SELECT contact_name, city FROM customers;
 
 -- 2. идентификатор заказа и разницу между датами формирования (order_date) заказа и его отгрузкой (shipped_date) из таблицы orders
-SELECT shipped_date - order_date AS lead_time FROM orders;
+SELECT order_id, shipped_date - order_date AS lead_time FROM orders;
 
 -- 3. все города без повторов, в которых зарегистрированы заказчики (customers)
 SELECT DISTINCT city FROM customers;
